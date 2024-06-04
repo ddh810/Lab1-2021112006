632bcf1936b4dde3baadc293373fc8c628c87cb0
ddh810 <Sci_fiction@outlook.com>
2024-06-05 01:40:37 +0800

    Lab3w

--- src/Main.java
+++ src/Main.java
@@ -181,41 +181,34 @@ public class Main {
         }*/
     }
     //***Q6
-    public static String randomWalk(){//打印fault5
-        List<String> list = new ArrayList<>(graph.keySet());
+    public static String randomWalk(String start){
+        String road = "A"; List<String> list = new ArrayList<>(graph.keySet());
         SecureRandom random = new SecureRandom();
-        String start = list.get(random.nextInt(list.size()));
-
         List<String> visitedEdges = new ArrayList<>();
         String current = start;
         List<String> paths = new ArrayList<>();
         boolean flag = false;
-        System.out.println("start:"+start);
-
-        while(!flag&&graph.containsKey(current)&&!graph.get(current).isEmpty()){
-            Map<String,Integer> edges = graph.get(current);
+        System.out.println("start:"+ start);
+        while(!flag && graph.containsKey(current) && !graph.get(current).isEmpty()){
+            Map<String, Integer> edges = graph.get(current);
             List<String> nextlist = new ArrayList<>(edges.keySet());
             String next = nextlist.get(random.nextInt(nextlist.size()));
             paths.add(current);
-
             String edge = current+"->"+next;
             if(visitedEdges.contains(edge)){
-                current = next;
+                current = next; road = "B";
                 break;
-            }//终止条件
+            }
             visitedEdges.add(edge);
             current = next;
-            //用户停止
             Scanner scanner = new Scanner(System.in);
             System.out.println("current: "+current+" ---enter'D'to end");
             if(scanner.next().equals("D")){
-                flag = true;
-            }
+                flag = true; road = "D";
+            }  else road = "C";
         }
         paths.add(current);//最后一个补上
         System.out.println("result:"+String.join(",",paths));
-
-        //写文件
         String filepath = "src/randomWalk.txt";
         File file = new File(filepath);
         try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
@@ -226,10 +219,10 @@ public class Main {
         }catch(IOException e){
             e.printStackTrace();
         }
-        return "";
+        return road;
     }
-    //***Q1
-    //文件读取创建
+//    //***Q1
+//    //文件读取创建
     public static Map<String, Map<String, Integer>> CreateDirectedGraph(String finalString) {
         Map<String, Map<String, Integer>> G = new HashMap<>();
         String[] words = finalString.split("\\s+");
@@ -242,174 +235,221 @@ public class Main {
         }
         return G;
     }
-    //***Q2
-    //直接打印
-    public static void ShowDirectedGraph(Map<String, Map<String, Integer>> G) {
-        for (Map.Entry<String, Map<String, Integer>> entry : G.entrySet()) {
-            String V1 = entry.getKey();
-            Map<String, Integer> E = entry.getValue();
-            System.out.println(V1+"->");
-            for (Map.Entry<String, Integer> e : E.entrySet()) {
-                String V2 = e.getKey();
-                Integer V3 = e.getValue();
-                System.out.println(V2+":"+V3+";");
-            }
-            System.out.println();
-        }
-    }
-    //***Q3
-    public static void BridgeWords() {
-        //判断是否存在
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter word1: ");
-        String word1 = scanner.nextLine().toLowerCase();
-        System.out.print("Enter word2: ");
-        String word2 = scanner.nextLine().toLowerCase();//读
-        if(!graph.containsKey(word1)) {
-            System.out.println("No "+word1+" found");
-            return;
-        }
-        if(!graph.containsKey(word2)) {
-            System.out.println("No "+word2+" found");
-            return;
-        }
-
-        //保存所有桥接词
-        List<String>bridgeWords = queryBridgeWords(word1,word2);
-        if(bridgeWords.isEmpty()) {
-            System.out.println("No bridge word from "+word1+" to "+word2);
-        }else{
-            System.out.println("Bridge words from "+word1+" to "+word2+" are:"+String.join(",",bridgeWords));
-        }//打印
-    }
-    //word1的邻接是否存在并且邻接的邻接是不是word2
-    public static List<String> queryBridgeWords(String word1, String word2){
-        word1 = word1.toLowerCase();
-        word2 = word2.toLowerCase();
-        List<String> bridgeWords = new ArrayList<>();
-        Map<String, Integer> word1Map = graph.get(word1);
-        if (word1Map == null) {
-            return bridgeWords;
-        } else {
-            for (String next : word1Map.keySet()){
-                if (graph.containsKey(next) && graph.get(next).containsKey(word2)) {
-                    bridgeWords.add(next);
-                }
-            }
-        }
-        return bridgeWords;
-    }
-    //***Q4
-    public static String generateNewText(String inputText){
-        //this.graph=G.getG();//fault3
-        StringBuilder result=new StringBuilder();
-        String[]Nwords=inputText.split("\\s+");
-        SecureRandom rand=new SecureRandom();
-        for(int i=0;i<Nwords.length-1;i++){
-            String word1=Nwords[i];
-            String word2=Nwords[i+1];
-            result.append(word1).append(" ");
-
-            List<String>bridgeWords = queryBridgeWords(word1,word2);//查找 拼接 打印
-            if(!bridgeWords.isEmpty()){
-                String choice=bridgeWords.get(rand.nextInt(bridgeWords.size()));
-                result.append(choice).append(" ");
-            }
-
-        }
-        result.append(Nwords[Nwords.length-1]).append(" ");//补上最后一个词
-        return result.toString();
-    }
-    //***Q5
-    public static String getShortestPath(String source, String destination) {
-        Set<String> visited = new HashSet<>();
-        Set<String> unvisited = new HashSet<>();
-        Map<String,Integer> distance = new HashMap<>();
-        Map<String,String> predecessor = new HashMap<>();
-
-        String result;
-
-        distance.put(source, 0);
-        unvisited.add(source);
-        while (!unvisited.isEmpty()) {
-            String current = getCurrent(unvisited,distance);
-            unvisited.remove(current);
-            for(Map.Entry<String,Integer> entry : graph.get(current).entrySet()) {
-                String neighbour = entry.getKey();
-                int distanceToNeighbour = entry.getValue();//下一访问节点信息
-
-                if(!visited.contains(neighbour)) {
-                    calcShortestPath(neighbour,distanceToNeighbour,current,distance,predecessor);
-                    unvisited.add(neighbour);
-                }
-            }
-            visited.add(current);
-        }
-        return genPath(predecessor,source,destination);
-    }
-    //获取最近节点
-    public static String getCurrent(Set<String> unvisited,Map<String,Integer> distance) {
-        String current = "";
-        int minDistance = Integer.MAX_VALUE;
-        for(String neighbour : unvisited) {
-            int distanceToNeighbour = distance.get(neighbour);
-            if(distanceToNeighbour < minDistance) {
-                minDistance = distanceToNeighbour;
-                current = neighbour;
-            }
-        }
-        return current;
-    }
-    //计算最小值
-    public static void calcShortestPath(String neighbour, int distance, String current, Map<String,Integer> distanceMap, Map<String,String> predecessor) {
-        int distanceToNeighbour= distanceMap.get(current);
-        if(distanceToNeighbour+distance<distanceMap.getOrDefault(neighbour,Integer.MAX_VALUE)){
-            distanceMap.put(neighbour,distanceToNeighbour+distance);
-            predecessor.put(neighbour,current);//fault4 局部最优到全局最优
-        }
-    }
-    public static String genPath(Map<String,String> predecessor, String source, String destination) {
-        String cur=destination;
-        LinkedList<String>path = new LinkedList<>();
-        if(!predecessor.containsKey(destination)) {
-            return "unreachable";
-        }
-        path.add(cur);
-        while(predecessor.containsKey(cur)) {
-            cur=predecessor.get(cur);
-            path.add(cur+"->");
-        }
-        Collections.reverse(path);
-        return path.toString();
-    }
-
-    public static Map<String,String>allPaths(String source) {
-        Set<String> visited = new HashSet<>();
-        Set<String> unvisited = new HashSet<>();
-        Map<String, Integer> distance = new HashMap<>();
-        Map<String, String> predecessor = new HashMap<>();
-        Map<String, String> allpaths = new HashMap<>();
-
-        distance.put(source, 0);
-        unvisited.add(source);
-
-        while (!unvisited.isEmpty()) {
-            String current = getCurrent(unvisited, distance);
-            unvisited.remove(current);
-            for (Map.Entry<String, Integer> entry : graph.get(current).entrySet()) {
-                String neighbour = entry.getKey();
-                int distanceToNeighbour = entry.getValue();
-                if (!visited.contains(neighbour)) {
-                    calcShortestPath(neighbour, distanceToNeighbour, current, distance, predecessor);
-                    unvisited.add(neighbour);
-                }
-            }
-            visited.add(current);
-        }
-
-        for (String node : graph.keySet()) {
-            allpaths.put(node, genPath(predecessor, source, node));
-        }
-        return allpaths;
-    }
+//    //***Q2
+//    //直接打印
+//    public static void ShowDirectedGraph(Map<String, Map<String, Integer>> G) {
+//        for (Map.Entry<String, Map<String, Integer>> entry : G.entrySet()) {
+//            String V1 = entry.getKey();
+//            Map<String, Integer> E = entry.getValue();
+//            System.out.println(V1+"->");
+//            for (Map.Entry<String, Integer> e : E.entrySet()) {
+//                String V2 = e.getKey();
+//                Integer V3 = e.getValue();
+//                System.out.println(V2+":"+V3+";");
+//            }
+//            System.out.println();
+//        }
+//    }
+//    //***Q3
+//    public static void BridgeWords() {
+//        //判断是否存在
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter word1: ");
+//        String word1 = scanner.nextLine().toLowerCase();
+//        System.out.print("Enter word2: ");
+//        String word2 = scanner.nextLine().toLowerCase();//读
+//        if(!graph.containsKey(word1)) {
+//            System.out.println("No "+word1+" found");
+//            return;
+//        }
+//        if(!graph.containsKey(word2)) {
+//            System.out.println("No "+word2+" found");
+//            return;
+//        }
+//
+//        //保存所有桥接词
+//        List<String>bridgeWords = queryBridgeWords(word1,word2);
+//        if(bridgeWords.isEmpty()) {
+//            System.out.println("No bridge word from "+word1+" to "+word2);
+//        }else{
+//            System.out.println("Bridge words from "+word1+" to "+word2+" are:"+String.join(",",bridgeWords));
+//        }//打印
+//    }
+//    //word1的邻接是否存在并且邻接的邻接是不是word2
+//    public static List<String> queryBridgeWords(String word1, String word2){
+//        word1 = word1.toLowerCase();
+//        word2 = word2.toLowerCase();
+//        List<String> bridgeWords = new ArrayList<>();
+//        Map<String, Integer> word1Map = graph.get(word1);
+//        if (word1Map == null) {
+//            return bridgeWords;
+//        } else {
+//            for (String next : word1Map.keySet()){
+//                if (graph.containsKey(next) && graph.get(next).containsKey(word2)) {
+//                    bridgeWords.add(next);
+//                }
+//            }
+//        }
+//        return bridgeWords;
+//    }
+//    //***Q4
+//    public static String generateNewText(String inputText){
+//        //this.graph=G.getG();//fault3
+//        StringBuilder result=new StringBuilder();
+//        String[]Nwords=inputText.split("\\s+");
+//        SecureRandom rand=new SecureRandom();
+//        for(int i=0;i<Nwords.length-1;i++){
+//            String word1=Nwords[i];
+//            String word2=Nwords[i+1];
+//            result.append(word1).append(" ");
+//
+//            List<String>bridgeWords = queryBridgeWords(word1,word2);//查找 拼接 打印
+//            if(!bridgeWords.isEmpty()){
+//                String choice=bridgeWords.get(rand.nextInt(bridgeWords.size()));
+//                result.append(choice).append(" ");
+//            }
+//
+//        }
+//        result.append(Nwords[Nwords.length-1]).append(" ");//补上最后一个词
+//        return result.toString();
+//    }
+//    //***Q5
+//    public static String getShortestPath(String source, String destination) {
+//        Set<String> visited = new HashSet<>();
+//        Set<String> unvisited = new HashSet<>();
+//        Map<String,Integer> distance = new HashMap<>();
+//        Map<String,String> predecessor = new HashMap<>();
+//
+//        String result;
+//
+//        distance.put(source, 0);
+//        unvisited.add(source);
+//        while (!unvisited.isEmpty()) {
+//            String current = getCurrent(unvisited,distance);
+//            unvisited.remove(current);
+//            for(Map.Entry<String,Integer> entry : graph.get(current).entrySet()) {
+//                String neighbour = entry.getKey();
+//                int distanceToNeighbour = entry.getValue();//下一访问节点信息
+//
+//                if(!visited.contains(neighbour)) {
+//                    calcShortestPath(neighbour,distanceToNeighbour,current,distance,predecessor);
+//                    unvisited.add(neighbour);
+//                }
+//            }
+//            visited.add(current);
+//        }
+//        return genPath(predecessor,source,destination);
+//    }
+//    //获取最近节点
+//    public static String getCurrent(Set<String> unvisited,Map<String,Integer> distance) {
+//        String current = "";
+//        int minDistance = Integer.MAX_VALUE;
+//        for(String neighbour : unvisited) {
+//            int distanceToNeighbour = distance.get(neighbour);
+//            if(distanceToNeighbour < minDistance) {
+//                minDistance = distanceToNeighbour;
+//                current = neighbour;
+//            }
+//        }
+//        return current;
+//    }
+//    //计算最小值
+//    public static void calcShortestPath(String neighbour, int distance, String current, Map<String,Integer> distanceMap, Map<String,String> predecessor) {
+//        int distanceToNeighbour= distanceMap.get(current);
+//        if(distanceToNeighbour+distance<distanceMap.getOrDefault(neighbour,Integer.MAX_VALUE)){
+//            distanceMap.put(neighbour,distanceToNeighbour+distance);
+//            predecessor.put(neighbour,current);//fault4 局部最优到全局最优
+//        }
+//    }
+//    public static String genPath(Map<String,String> predecessor, String source, String destination) {
+//        String cur=destination;
+//        LinkedList<String>path = new LinkedList<>();
+//        if(!predecessor.containsKey(destination)) {
+//            return "unreachable";
+//        }
+//        path.add(cur);
+//        while(predecessor.containsKey(cur)) {
+//            cur=predecessor.get(cur);
+//            path.add(cur+"->");
+//        }
+//        Collections.reverse(path);
+//        return path.toString();
+//    }
+//
+//    public static Map<String,String>allPaths(String source) {
+//        Set<String> visited = new HashSet<>();
+//        Set<String> unvisited = new HashSet<>();
+//        Map<String, Integer> distance = new HashMap<>();
+//        Map<String, String> predecessor = new HashMap<>();
+//        Map<String, String> allpaths = new HashMap<>();
+//
+//        distance.put(source, 0);
+//        unvisited.add(source);
+//
+//        while (!unvisited.isEmpty()) {
+//            String current = getCurrent(unvisited, distance);
+//            unvisited.remove(current);
+//            for (Map.Entry<String, Integer> entry : graph.get(current).entrySet()) {
+//                String neighbour = entry.getKey();
+//                int distanceToNeighbour = entry.getValue();
+//                if (!visited.contains(neighbour)) {
+//                    calcShortestPath(neighbour, distanceToNeighbour, current, distance, predecessor);
+//                    unvisited.add(neighbour);
+//                }
+//            }
+//            visited.add(current);
+//        }
+//
+//        for (String node : graph.keySet()) {
+//            allpaths.put(node, genPath(predecessor, source, node));
+//        }
+//        return allpaths;
+//    }
+//    public static String randomWalk(){
+//        List<String> list = new ArrayList<>(graph.keySet());
+//        SecureRandom random = new SecureRandom();
+//        String start = list.get(random.nextInt(list.size()));
+//
+//        List<String> visitedEdges = new ArrayList<>();
+//        String current = start;
+//        List<String> paths = new ArrayList<>();
+//        boolean flag = false;
+//        System.out.println("start:"+ start);
+//
+//        while(!flag && graph.containsKey(current) && !graph.get(current).isEmpty()){
+//            Map<String, Integer> edges = graph.get(current);
+//            List<String> nextlist = new ArrayList<>(edges.keySet());
+//            String next = nextlist.get(random.nextInt(nextlist.size()));
+//            paths.add(current);
+//
+//            String edge = current+"->"+next;
+//            if(visitedEdges.contains(edge)){
+//                current = next;
+//                break;
+//            }//终止条件
+//            visitedEdges.add(edge);
+//            current = next;
+//            //用户停止
+//            Scanner scanner = new Scanner(System.in);
+//            System.out.println("current: "+current+" ---enter'D'to end");
+//            if(scanner.next().equals("D")){
+//                flag = true;
+//            }
+//        }
+//        paths.add(current);//最后一个补上
+//        System.out.println("result:"+String.join(",",paths));
+//
+//        //写文件
+//        String filepath = "src/randomWalk.txt";
+//        File file = new File(filepath);
+//        try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+//            for(String line : paths){
+//                bufferedWriter.write(line+" ");
+//            }
+//            System.out.println("Done");
+//        }catch(IOException e){
+//            e.printStackTrace();
+//        }
+//        return "";
+//    }
 }
